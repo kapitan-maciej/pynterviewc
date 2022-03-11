b435a8506dc683e53d84608c1ed3ae58437d8094
Maciej Szczepaniak <maciej.szczepaniak@intel.com>
2022-03-11 09:29:12 +0100

    task-2

---
+++ task-2/main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <signal.h>
+
+static bool g_condition = false;
+
+void signal_handler(int signum)
+{
+    printf("got signal %d\n", signum);
+    g_condition = signum % 2;
+}
+
+void *bus_watcher(void *ctx)
+{
+    while (!g_condition)
+    {
+        ;
+    }
+
+    printf("condition set\n");
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t thread;
+
+    signal(SIGBUS, signal_handler);
+    signal(SIGUSR1, signal_handler);
+
+    pthread_create(&thread, NULL, bus_watcher, NULL);
+    pthread_join(thread, NULL);
+
+    // Depending on how was this program built we may never join
+    // @todo:
+    // 1. Why and how do we prevent that.
+    // 2. Fix
+
+    return 0;
+}
